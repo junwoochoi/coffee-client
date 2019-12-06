1becd1f18e8f778c9b2841665f2e21cbb50239c3
최준우 <junwoochoi@zuminternet.com>
2019-12-06 15:11:16 +0900

    Initial commit

---
+++ src/main/java/com/thehecklers/coffeeclient/CoffeeClientApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.coffeeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeClientApplication.class, args);
+	}
+
+}
